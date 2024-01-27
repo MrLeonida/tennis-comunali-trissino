b163d6c4fffef3f07345d754234791ed7c325e14
Matteo Vistosi <matteovistosi@MacBook-Pro-di-Matteo-Vistosi.local>
2024-01-27 17:18:32 +0100

    Dev commit.

--- src/app/associazione/page.js
+++ src/app/associazione/page.js
@@ -85,13 +85,11 @@ export default function Page() {
           <div className="mx-auto max-w-7xl px-6 py-32 sm:py-40 lg:px-8">
             <div className="mx-auto max-w-2xl lg:mx-0 lg:grid lg:max-w-none lg:grid-cols-2 lg:gap-x-16 lg:gap-y-6 xl:grid-cols-1 xl:grid-rows-1 xl:gap-x-8">
               <h1 className="max-w-2xl text-4xl font-bold tracking-tight text-gray-900 sm:text-6xl lg:col-span-2 xl:col-auto">
-                We’re a passionate group of people working from around the world to build the future of ecommerce.
+                Dopolavoro Ferroviario: Radici solide, iniziative dinamiche, un'unione di soci in continua crescita.
               </h1>
               <div className="mt-6 max-w-xl lg:mt-0 xl:col-end-1 xl:row-start-1">
                 <p className="text-lg leading-8 text-gray-600">
-                  Anim aute id magna aliqua ad ad non deserunt sunt. Qui irure qui lorem cupidatat commodo. Elit sunt
-                  amet fugiat veniam occaecat fugiat aliqua. Anim aute id magna aliqua ad ad non deserunt sunt. Qui
-                  irure qui lorem cupidatat commodo.
+                  Attraverso iniziative sportive, culturali, e ricreative, miriamo a potenziare l'utilizzo del tempo libero, migliorare le condizioni di crescita culturale e sociale degli iscritti, e favorire l'armonizzazione della vita associativa attraverso lo scambio di valori ed esperienze, coinvolgendo non solo gli associati ma anche le loro famiglie.
                 </p>
               </div>
               <img
